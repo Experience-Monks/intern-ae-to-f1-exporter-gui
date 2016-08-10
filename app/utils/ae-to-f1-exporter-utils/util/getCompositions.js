c1c87fb871a990bc3e54fd8fb1cdf889f0473b22
nascherman <n_scherman@hotmail.com>
2016-08-10 18:02:55 -0400

    remove unused utils

--- app/utils/ae-to-f1-exporter-utils/util/getCompositions.js
+++
@@ -1,9 +0,0 @@
-var isComposition = require('./isComposition');
-
-// this will return all compositions from the json
-module.exports = function(json) {
-  var items = json.project.items;
-
-  // loop through all footage compositions etc
-  return items.filter(isComposition);
-};
--- app/utils/ae-to-f1-exporter-utils/util/isComposition.js
+++
@@ -1,4 +0,0 @@
-// given an item this function will return whether the item is a Composition
-module.exports = function isComposition(item) {
-  return item.typeName === 'Composition';
-};
